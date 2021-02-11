c624832d6b41164dc31ecc67f23d8cd6f65157f2
Jose Olivares <joliva21@uic.edu>
2021-02-11 04:43:04 -0600

    third commit

--- sketch.js
+++ sketch.js
@@ -7,42 +7,67 @@ let speed = 15;
 let speedy = 20;
 let angle = 250;
 
+
+
 function setup(){
-    alert("I wanted the user to be able to manipulate the shape and the color of the square: When any key is pressed it inverts the square colors and when the mouse is moved in th x or y position it will increase the size and lenght of the squares. ");
+    alert("The formal elements of this preoject are color and the movement of the squares. The user will be able to manipulate the size shape and colors of squares.\nHere are the controls: \n - Mouse Position will change the size of the shaking square \n - UP Arrow (pink & purple) \n - Down Arrow (purple & pink) \n - Left Arrow (yello & blue) \n - Right Arrow (blue & yellow) \n - Any Other key (all black)");
     createCanvas (windowWidth,windowHeight);
     PosX = random(0, windowWidth);
     PosY = random(0, windowHeight);
-    squareX = 10;
-    squareY = 8;
-    background(151, 68, 230); 
-    ///background(156, 222, 236);
+    squareX = 35;
+    squareY = 35;
+    background (0);
+    
 }
 
 function draw(){
+    
     let r1 = map(mouseY, 0, width, 0, height);
     let r2 = map(mouseX, 0, width, 0, height);
-    
-  stroke(112);
-  
-    translate(PosX,PosY);
    
-    if (keyIsPressed === true) {
-        fill(235, 165, 191);
-      } else  {
-        fill(209, 213, 242);
+    translate(PosX,PosY);
+    fill(0);
+    stroke(120);
+///colors for square
+    if (keyCode === UP_ARROW) 
+    {
+        fill(121, 72, 162);
+      } else if (keyCode === DOWN_ARROW)  
+      {
+        fill(232,142,214);
       } 
-      circle(squareX,squareY,squareSize);
-  
-    rotate(angle);
-  
-    
-   if (keyIsPressed === true)  {
-       fill (209, 213, 242);
-   }else {
-    fill(235, 165, 191);
+    if (keyCode === LEFT_ARROW)
+    {
+      fill(147, 196, 209);
+    } else if (keyCode === RIGHT_ARROW)
+    {
+     fill(252, 228, 180);
    }
-    rect(squareX,squareY,r2,r1)
+
+     square(squareX,squareY,squareSize);
+      
+/// colors for rectangle 
+      fill(0);
+    if (keyCode === UP_ARROW) 
+    {
+        fill(232,142,214);
+      } else if (keyCode === DOWN_ARROW)  
+      {
+        fill(121, 72, 162);
+      } 
+      
+    if (keyCode === LEFT_ARROW)
+    {
+       fill(252, 228, 180);
+    } else if  (keyCode === RIGHT_ARROW)
+       fill(147, 196, 209);
+   {
+
+   }
+    rotate(angle);
+      rect(squareX,squareY,r2,r1)
   
+   
    if (PosX > windowWidth - 25 || PosX < 25 ) {     
         speed = speed * -1; 
             
@@ -51,17 +76,13 @@ function draw(){
         speedy = speedy * -1;
          
     }    
+    
     PosX = PosX + speed;
     PosY = PosY + speedy; 
-    angle = angle + .5;
+    angle = angle + .1;
     
 }
-function keyPressed() {
-    if (keyCode === LEFT_ARROW) {
-      PosX=PosY+20+ speed;
-    } else if (keyCode === RIGHT_ARROW) {
-    PosX = PosX;
-    }
+
   
- }
+
 
